b0f5437f4fd36eea88f1bc2d4a9e79ba57e04a4c
graves-hj <hjgraves@proton.me>
2026-07-04 15:24:26 -0700

    we go againe

--- client/src/pages/Splash.jsx
+++ client/src/pages/Splash.jsx
@@ -68,7 +68,7 @@ const Splash = ({callback}) => {
           }`}
           style={{ animationDelay: '800ms' }}
         >
-          THE MOST RADICAL SPOT ON THE WEB 🌈
+          THE MOST RADICAddL SPOT ON THE WEB 🌈
         </p>
 
         {/* Buttons */}
