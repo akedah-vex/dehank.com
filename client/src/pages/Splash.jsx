a8f4c71b418c17a75d2b4153ce803b808875a14f
graves-hj <hjgraves@proton.me>
2026-07-04 15:17:51 -0700

    struggle bus 2.0

--- client/src/pages/Splash.jsx
+++ client/src/pages/Splash.jsx
@@ -68,7 +68,7 @@ const Splash = ({callback}) => {
           }`}
           style={{ animationDelay: '800ms' }}
         >
-          THE MOST RADICAAAL SPOT ON THE WEB 🌈
+          THE MOST RADICAL SPOT ON THE WEB 🌈
         </p>
 
         {/* Buttons */}
