0d4dc023be33df50adff26b29d5b892faf7bee2f
graves-hj <hjgraves@proton.me>
2026-07-05 14:07:03 -0700

    remove base uri

--- server/src/Auth.js
+++ server/src/Auth.js
@@ -11,7 +11,7 @@ const login = async (req, res) => {
   console.log("Received login request for username:", username);
 
   if (!username || !password) {
-    console.log("Missing username or password in request bodyy");
+    console.log("Missing username or password in request body");
     return res
       .status(400)
       .json({ message: "Please enter both username and password." });
