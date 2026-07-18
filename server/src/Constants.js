44b7457dd7deed6db5de12046051a5b80326bf12
graves-hj <hjgraves@proton.me>
2026-07-17 22:29:19 -0700

    fix database pathing, fix typo

--- server/src/Constants.js
+++ server/src/Constants.js
@@ -1,6 +1,6 @@
-export const databaseFilePath = "../database/db.json";
-export const usersDatabase = "../database/users.db.json";
-export const quipsDatabase = "../database/quips.db.json";
+export const databaseFilePath = "../../database/db.json";
+export const usersDatabase = "../../database/users.db.json";
+export const quipsDatabase = "../../database/quips.db.json";
 
 export const endpoints = {
   API_AUTH_LOGIN: "/api/auth/login",
