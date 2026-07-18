38d6bd8114e08bae6f597cc9bd83207c60bd19ab
graves-hj <hjgraves@proton.me>
2026-07-17 20:33:45 -0700

    adjusted modal banner ui

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = ({ children, isOpen=null, onClose=null }) => {
         className="fixed inset-0 bg-black/80 z-50 flex items-center justify-center p-6"
         onClick={onClose}
         >
-            <div className="absolute top-100 right-185 bg-yellow-400 text-black text-lg font-black px-6 py-1 border-4 border-black -rotate-15 shadow-md z-10 animate-[popIn_0.5s_backwards]">
+            <div className="absolute top-100 right-265 bg-yellow-400 text-black text-lg font-black px-6 py-1 border-4 border-black -rotate-15 shadow-md z-10 animate-[popIn_0.5s_backwards]">
             CONFIDENTIAL
             </div>
         <div 
@@ -19,7 +19,7 @@ const Modal = ({ children, isOpen=null, onClose=null }) => {
             onClick={onClose}
             className="absolute top-4 right-4 bg-black text-white border-4 border-black px-4 py-2 font-black text-xl hover:bg-red-600 hover:border-red-600 active:scale-95 transition-all"
             >
-            ✕
+            x
             </button>
 
             <div className="p-10 pt-12">
